0f6fbdd568a949c8a1d6c374bb51cfa0fbf00074
Max <ip.developer58@gmail.com>
2019-08-22 15:42:29 +0300

    статус 200, работа над маской

--- js/dist/main.js
+++ js/dist/main.js
@@ -1,5 +1,6 @@
-//Не работают маски на ДР и телефон
+//при добавлении маски не обновляются реактивно данные(связать v-model маски с действующим)
 //отправляй на сервак новых пользователей
+//должен парсить id пользователя
 //сортировка по дате рождения(попробуй moment)
 
 
@@ -25,123 +26,90 @@ fetch('https://urozaev.github.io/showTest/users.json').then(
     });
   });
 
-Vue.component('user-component', { 
-    template: `
+// Vue.component('user-component', { 
+//     template: `
       
-        <div class="user__info">
-            <div class="content">
-                    <span><strong>{{user.name}}</strong></span>
-                    <br>
-                    <span>ID: {{user.id}}</span>
-                    <br>
-                    <span>Специальность: {{user.role}}</span>
-                    <br>
-                    <span> Номер телефона: {{user.phone}}</span>
-            </div>
-            <slot name="user-modal">
+//         <div class="user__info">
+//             <div class="content">
+//                     <span><strong>{{user.name}}</strong></span>
+//                     <br>
+//                     <span>ID: {{user.id}}</span>
+//                     <br>
+//                     <span>Специальность: {{user.role}}</span>
+//                     <br>
+//                     <span> Номер телефона: {{user.phone}}</span>
+//             </div>
+//             <slot name="user-modal">
             
-            </slot>
-        </div>
+//             </slot>
+//         </div>
       
-    `,
-    props: {
-      user: Object
-    }
-  });
+//     `,
+//     props: {
+//       user: Object
+//     }
+//   });
   
-  Vue.component('modal', {
-    template: '#modal-template'
-  })
+  // Vue.component('modal', {
+  //   template: '#modal-template'
+  // })
+
 
 var vm = new Vue({
 	el: '#app',
 	data: function() {
     return {
-    // users,
-    isModalVisible: false,
 		items: users,
     item: {name: '',phone: '',birthday: '',role: '',archive: ''},
-    // item: function(index) {
-    //   return {
-    //     name: index.name,
-    //     phone: index.phone,
-    //     birthday: index.birthday,
-    //     role: index.role,
-    //     archive: index.isArchive
-    //   }
-    // },
     edit: false,
     editIndex:-1
     }
 	},
-
+  // mounted: function() {
+  //   var im = new Inputmask("+7 (999) 999-9999");
+  //   im.mask(this.$refs.field);
+  // },
 	methods: {
 		addItem: function(e) {
 			e.preventDefault();
-      
       if(!this.edit)
         {
           this.items.push(this.item);
-          // this.item = {name: '',phone: ''};
           this.item = {name: '',phone: '',birthday: '',role: '',archive: ''};
-          // function submitForm() {
-          //   fetch("https://api2.esetnod32.ru/frontend/test/", {
-          //     method: "POST",
-          //     body: JSON.stringify(this.item)
-          //   })
-          // }
-          // fetch('https://urozaev.github.io/showTest/users.json', {
-          //   method: 'POST', // или 'PUT'
-          //   body: JSON.stringify('kjhj'), // data может быть типа `string` или {object}!
-          //   headers:{'Content-Type': 'application/json; charset=utf-8'}
-          // })
-          // userForm.onsubmit = async (e) => {
-          //   e.preventDefault();
-        
-          //   let response = await fetch('https://api2.esetnod32.ru/frontend/test/', {
-          //     method: 'POST',
-          //     body: new FormData(userForm)
-          //   });
-        
-          //   let result = await response.json();
-        
-          //   alert(result.message);
-          // };
         } 
       else 
         {
           this.items[this.editIndex] = this.item;
-          // this.item = {name: '',phone: ''};
           this.item = {name: '',phone: '',birthday: '',role: '',archive: ''};
           this.edit = false;
           this.editIndex = -1;
         }
 
-          fetch('https://api2.esetnod32.ru/frontend/test/', {
-            body: new FormData(userForm),
-            credentials: 'same-origin',
-            method: 'POST'
-          })
-          .then((res) => {
-            if(200 <= res.status && res.status < 300) {
-              return res;
-            }
-            throw new Error(response.statusText);
-          })
-          .then((res) => { return res.json()})
-          .then((data) => {console.log(data)})
-          .catch((error) => {console.log(error)})
+        fetch('https://api2.esetnod32.ru/frontend/test/', {
+          body: JSON.stringify(this.item),
+          credentials: 'same-origin',
+          method: 'POST'
+        })
+        .then((res) => {
+          if(200 <= res.status && res.status < 300) {
+            return res;
+          }
+          throw new Error(response.statusText);
+        })
+        // .then(function(response) {
+        //   return response.json();
+        // })
+        .then((data) => {console.log(data)})
+        .catch((error) => {console.log(error)})
           
       
-			$('#modal').modal('hide');
+      $('#modal').modal('hide');
+
 		},
-		// removeItem: function(index) {
-		// 	this.items.$remove(index);
-		// },
 		editItem: function(index) {
     	this.edit = true;
       this.editIndex = index;
-  	  this.item = this.items[index];
+      this.item = this.items[index];
       $('#modal').modal('show');
 		},
     editCancel: function(index){
@@ -199,50 +167,6 @@ var vm = new Vue({
   }
 });
 
-
-
-// $(function() {
-//   var button = $("button");
-//   var name = $("input[name=name]");
-
-//   name.keyup(function() {
-//     if (name.val().length > 0) {
-//       button.addClass('active');
-//     } else {
-//       button.removeClass('active');
-//     }
-//   });
-
-//   $("form").submit(function(event) {
-//     event.preventDefault();
-
-//     //get the form data
-//     const formData = {
-//       name: $("#user-name").val(),
-//       email: $("#user-phone").val(),
-//       caps: $("#user-birthday").val()
-//     };
-
-//     // process the form
-//     $.ajax({
-//       type: "POST",
-//       url: "https://api2.esetnod32.ru/frontend/test/",
-//       data: formData,
-//       dataType: "json",
-//       encode: true
-//     }).done(function(data) {
-//       $(".response")
-//         .empty()
-//         .append(JSON.stringify(data, null, 2));
-//     });
-//   });
-// });
-
-
-
-
-
-
 //Active elements
 const setActive = el => {
   [...el.parentElement.children].forEach(sib => sib.classList.remove('active'))
@@ -251,8 +175,3 @@ const setActive = el => {
 
 let active = [...document.body.querySelectorAll('.sort-button')]
 active.forEach(el => el.addEventListener('click', e => setActive(el)))
-
-//Masks
-//С МАСКАМИ ПОЧЕМУ ТО НЕ ВЕРНО ОТОБРАЖАЮТСЯ ДАННЫЕ ЮЗЕРА ПОСЛЕ ПРАВКИ
-$(".phone_mask").mask("+7 (999) 999-99-99");
-$('.mask-date').mask('99.99.9999');
