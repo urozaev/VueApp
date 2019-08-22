908f65aa95f199b1c813b9d658373b44792b02f0
Max Urozaev <maxim.urozaev@gmail.com>
2019-08-22 23:00:20 +0300

    перфинал коммит

--- js/dist/main.js
+++ js/dist/main.js
@@ -1,61 +1,12 @@
 //при добавлении маски не обновляются реактивно данные(связать v-model маски с действующим)
-//отправляй на сервак новых пользователей
 //должен парсить id пользователя
-//сортировка по дате рождения(попробуй moment)
 
 
 'use strict';
 
 const users = []
-const userForm = document.getElementById('userForm');
 
-fetch('https://urozaev.github.io/showTest/users.json').then(
-  successResponse => {
-    if (successResponse.status != 200) {
-      return null;
-    } else {
-      return successResponse.json();
-    }
-  },
-  failResponse => {
-    return null;
-  })
-  .then(function(json) {
-    json.forEach(element => {
-      users.push(element)
-    });
-  });
-
-// Vue.component('user-component', { 
-//     template: `
-      
-//         <div class="user__info">
-//             <div class="content">
-//                     <span><strong>{{user.name}}</strong></span>
-//                     <br>
-//                     <span>ID: {{user.id}}</span>
-//                     <br>
-//                     <span>Специальность: {{user.role}}</span>
-//                     <br>
-//                     <span> Номер телефона: {{user.phone}}</span>
-//             </div>
-//             <slot name="user-modal">
-            
-//             </slot>
-//         </div>
-      
-//     `,
-//     props: {
-//       user: Object
-//     }
-//   });
-  
-  // Vue.component('modal', {
-  //   template: '#modal-template'
-  // })
-
-
-var vm = new Vue({
+const vm = new Vue({
 	el: '#app',
 	data: function() {
     return {
@@ -65,10 +16,28 @@ var vm = new Vue({
     editIndex:-1
     }
 	},
-  // mounted: function() {
-  //   var im = new Inputmask("+7 (999) 999-9999");
-  //   im.mask(this.$refs.field);
-  // },
+  mounted: function() {
+    fetch('https://urozaev.github.io/showTest/users.json')
+    .then((res) => {
+      if(200 <= res.status && res.status < 300) {
+        return res;
+      }
+      throw new Error(response.statusText);
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      json.forEach(element => {
+        users.push(element)
+      });
+    })
+    .catch((error) => {console.log(error)})
+    var imp = new Inputmask("+7 (999) 999-9999");
+    imp.mask(this.$refs.field);
+    var imd = new Inputmask("01/01/2001");
+    imd.mask(this.$refs.datemask);
+  },
 	methods: {
 		addItem: function(e) {
 			e.preventDefault();
@@ -96,9 +65,6 @@ var vm = new Vue({
           }
           throw new Error(response.statusText);
         })
-        // .then(function(response) {
-        //   return response.json();
-        // })
         .then((data) => {console.log(data)})
         .catch((error) => {console.log(error)})
           
@@ -127,36 +93,20 @@ var vm = new Vue({
       })
     },
     filterDeveloper() {
-        this.items = _.filter(users, function (item) {
-            return item.role === 'developer';
-          })
+      this.items = _.filter(users, function (item) {
+          return item.role === 'developer';
+        })
     },
     filterManager() {
-        this.items = _.filter(users, function (item) {
-            return item.role === 'manager';
-          })
+      this.items = _.filter(users, function (item) {
+          return item.role === 'manager';
+        })
     },
     sortByName() {
       this.items = _.sortBy(this.items, ['name']);
     },
     sortByBDate() {
       this.items = _.sortBy(this.items, ['birthday']);
-      // this.items = _.sortBy(this.items, function(e) {
-      //   return new Date(e.birthday);
-      // });
-          // this.items.sort(function(a, b) {
-          //   let dateA = new Date(a.birthday);
-          //   let dateB = new Date(b.birthday);
-          //   console.log(dateA)
-          //   return dateA - dateB;
-          // });
-        // function formatDate(date) {
-         
-        // this.items = function(a) {
-        //   console.log(a)
-        //   return Intl.DateTimeFormat('ru').format(a.birthday);
-        // }  
-        // }
     },
     sortByID() {
         this.items = _.sortBy(this.items, ['id']);
